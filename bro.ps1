766618ee1020c576cbac927dd0bf5fe6d028843b
Adrian Belen <maradrianbelen@gmail.com>
2023-11-24 17:27:06 +0800

    Update bro.ps1
    do some changes and correct errors on retrieving  ACEs  DownloadACE,
    fix error in  Get-DomainObject function; get samaccountname instead

--- bro.ps1
+++ bro.ps1
@@ -15,6 +15,94 @@ COMMAND TO EXECUTE FOR EXZMPLE
 THE `#` IN THE LOGS IS HEADER OR SECTION FOR DIFFERENT GROUPS OF COMMANDS
 
 #>
+#DEFINE FUNCTION HERE
+#list all user/computer, and saved it in the directory
+function Get-DomainObject {
+	param(
+		[string]$ObjectType
+	)
+	$objects = @()
+	if ($ObjectType -eq "User") {
+		Write-Host "List all Domain users"
+		Write-host "# List all Domain users" |  Out-File -FilePath $filePath -Append
+		$users = Get-ADUser -Filter * | Select-Object SamAccountName
+		foreach ($user in $users) {
+			$name = $user.SamAccountName
+			$name | Out-File -Append -FilePath domainUsers.txt
+			$objects += $name
+		}
+	}
+ elseif ($ObjectType -eq "Computer") {
+		Write-Host "List all Domain Computers"
+		Write-host "# List all Domain Computers" |  Out-File -FilePath $filePath -Append
+		$computers = Get-ADComputer -Filter * | Select-Object Name
+		foreach ($computer in $computers) {
+			$name = $computer.Name
+			$name | Out-File -Append -FilePath domainComputers.txt
+			$objects += $name
+		}
+	}
+ else {
+		Write-Host "Invalid object type. Please specify 'User' or 'Computer'."
+	}
+ return $objects
+}
+function DownloadACE {
+	param(
+		[string[]]$objects,
+		[bool]$User #set this to true to donwload users
+	)
+	$targetDomain = $DomainName -split "\."
+	Write-Host $DomainName
+	Write-Host "TargetDomain: "
+	Write-Host $targetDomain[0]
+	Write-Host $targetDomain[1]
+	foreach ($object in $objects) {
+		Write-Host "_________NEW-OBJECT" |  Out-File -FilePath "ACES.txt" -Append
+		Write-Host "Object value: $object"
+		$first = $targetDomain[0]
+		$second = $targetDomain[1]
+		if ($user) {
+			$dn = "CN=$object,CN=Users,DC=$first,DC=$second"
+			Write-Host $dn
+			dsacls $dn |  Out-File -FilePath "ACES.txt" -Append
+		}
+		else {
+			$dn = "CN=$object,CN=Computers,DC=$first,DC=$second"
+			dsacls $dn | Out-File -FilePath "ACES.txt" -Append
+		}
+	
+	}
+
+}
+function RetrieveObject {
+	#retrieve Objects(users and computers) and filter it `write` permssion
+	param(
+		[string]$filename,
+		[string]$username
+	)
+	$filePath = $filename
+	$fileContent = Get-Content -Path $filePath
+	# Iterate through each line in the file
+	for ($i = 0; $i -lt $fileContent.Length; $i++) {
+		$currentLine = $fileContent[$i]
+		$allowRegex = "^Allow (.*{0})" -f $username
+		# Check if the current line starts with "Allow"
+		if ($currentLine -match $allowRegex) {
+			$user = $matches[0]
+			# Check the next line for the presence of "WRITE"
+			$nextLine = $fileContent[$i + 1]
+
+			if ($nextLine -match "WRITE") {
+				Write-Output "Trigger: $currentLine"
+				Write-Output  "Selected User: $user"
+				Write-Output "Following Line: $nextLine`n"
+			}
+		}
+	}
+
+}
+
 $text = "# POWERSHELL VERSION"
 $filePath = "output.txt"
 # Use Out-File to append the text to the file
@@ -56,14 +144,14 @@ else {
 }
 $text = "#USING WMI TO GATHER INFORMATION SUCH AS ANTIVIRUS, LIST UPDATE, PASSWORD IN TEXT FILES, AND USER ACCOUNT" |  Out-File -FilePath $filePath -Append
 
-wmic /namespace:\\root\securitycenter2 path antivirusproduct GET displayName,productState,pathToSignedProductExe |  Out-File -FilePath $filePath -Append
+wmic /namespace:\\root\securitycenter2 path antivirusproduct GET displayName, productState, pathToSignedProductExe |  Out-File -FilePath $filePath -Append
 wmic qfe list brief |  Out-File -FilePath $filePath -Append
-wmic DATAFILE where "drive='C:' AND Name like '%password%'" GET Name,readable,size /VALUE |  Out-File -FilePath $filePath -Append
+wmic DATAFILE where "drive='C:' AND Name like '%password%'" GET Name, readable, size /VALUE |  Out-File -FilePath $filePath -Append
 wmic useraccount list |  Out-File -FilePath $filePath -Append
 
 $text = "#GATHERING INFORMATION ON WMIC. Domain Information, list all users, groups, member of domain admins groups, and list all computer" |  Out-File -FilePath $filePath -Append
 $text = "Domains" |  Out-File -FilePath $filePath -Append
-wmic NTDOMAIN GET DomainControllerAddress,DomainName,Roles |  Out-File -FilePath $filePath -Append
+wmic NTDOMAIN GET DomainControllerAddress, DomainName, Roles |  Out-File -FilePath $filePath -Append
 $text = "list domain users" |  Out-File -FilePath $filePath -Append
 wmic /NAMESPACE:\\root\directory\ldap PATH ds_user GET ds_samaccountname |  Out-File -FilePath $filePath -Append
 $text = "list domain groups" |  Out-File -FilePath $filePath -Append
@@ -80,7 +168,7 @@ $VMAdapter = Get-WmiObject Win32_NetworkAdapter -Filter 'Manufacturer LIKE "VMwa
 $VMBios = Get-WmiObject Win32_BIOS -Filter 'SerialNumber LIKE
 "%VMware%"'
 $VMToolsRunning = Get-WmiObject Win32_Process -Filter 'Name="vmtoolsd.exe"'
-if([Bool] ($VMAdapter -or $VMBios -or $VMToolsRunning)){
+if ([Bool] ($VMAdapter -or $VMBios -or $VMToolsRunning)) {
 	$text = "you are inside of vmware thing " |  Out-File -FilePath $filePath -Append
 }
 $text = "# RECON USING ADSI"
@@ -116,9 +204,9 @@ else {
 	$computer = Get-DomainObject -ObjectType "Computer"
 	Write-host $computer |  Out-File -FilePath $filePath -Append
 	Write-Host "#List Delegation vulnerabilities" | Out-File -FilePath $filePath -Append
-	$text = "List computer that is trusted for delegation, UNconstrained Delegation" |  Out-File -FilePath $filePath -Append
+	Write-Host "## List computer that is trusted for delegation, UNconstrained Delegation" |  Out-File -FilePath $filePath -Append
 	Get-ADObject -Filter { TrustedForDelegation -eq $True } |  Out-File -FilePath $filePath -Append
-	$text = "List User that is trusted for delegation, constrained Delegation(checking the SPN(msDS-AllowedToDelegateTo))" |  Out-File -FilePath $filePath -Append
+	Write-Host "## List User that is trusted for delegation, constrained Delegation(checking the SPN(msDS-AllowedToDelegateTo))" |  Out-File -FilePath $filePath -Append
 	Get-ADObject -Filter { msDS-AllowedToDelegateTo -ne "$null" } -Properties msDS-AllowedToDelegateTo | Out-File -FilePath $filePath -Append
 	#check for local admin privs
 	# Check if the current user is a member of the local administrators group
@@ -134,82 +222,24 @@ else {
 		Write-Host $text
 		$text |  Out-File -FilePath $filePath -Append
 	}
-	$text = "Resource Based Delegation" |  Out-File -FilePath $filePath -Append
+	Write-Host "## Resource Based Delegation" |  Out-File -FilePath $filePath -Append
 	#access the saved users and computer. domainUsers.txt & domainComputers.txt
-	$users = RetrieveObject -filename "domainUsers.txt"
-	$computers = RetrieveObject -filename 'DomainComuters.txt'
-	$allObject=$users+$computers;
-							#######################################
-							####THIS IS THE TWO CONDITION, BRO#####
-							#######################################
+	$users = Get-Content -Path "domainUsers.txt"
+	$computers = Get-Content -Path 'DomainComputers.txt'
+	#######################################
+	####These are THE TWO CONDITION, BRO#####
+	#######################################
 	#list user that has write permission on resource based pc using dc  dsacls "CN=BOB-PC-1,CN=Computers,DC=nukesec,DC=lab" > output.txt
-	DownloadACE -object $allObject -User $true#download ace
-											#retrieve the ACES.TXT
-											#check for the write permission
-
+	DownloadACE -objects $users -User $true #download ace
+	DownloadACE -objects $computers #download ace
+	#retrieve the ACES.TXT with `write` permission
+	RetrieveObject -filename "ACES.TXT" | Out-File -FilePath $filePath -Append
 	#then check if `msDS-AllowedToActOnBehalfOfOtherIdentity` property of resource based pc is null
-	# Get all domain objects with msDS-AllowedToActOnBehalfOfOtherIdentity set to null
-	# Get all computers with resource-based constrained delegation enabled
 	$computers = Get-ADComputer -Filter { msDS-AllowedToActOnBehalfOfOtherIdentity -notlike '*' } |  Out-File -FilePath $filePath -Append
 	# Get all users with resource-based constrained delegation enabled
 	$users = Get-ADUser -Filter { msDS-AllowedToActOnBehalfOfOtherIdentity -notlike '*' } |  Out-File -FilePath $filePath -Append
+
 	
 
 
-}
-#list all user/computer, and saved it in the directory
-function Get-DomainObject {
-	param(
-		[string]$ObjectType
-	)
-	$objects = @()
-	if ($ObjectType -eq "User") {
-		Write-Host "List all Domain users"
-		Write-host "# List all Domain users" |  Out-File -FilePath $filePath -Append
-		$users = Get-ADUser -Filter * | Select-Object Name
-		foreach ($user in $users) {
-			$name = $user.Name
-			$name | Out-File -Append -FilePath domainUsers.txt
-			$objects += $name
-		}
-	}
- elseif ($ObjectType -eq "Computer") {
-		Write-Host "List all Domain Computers"
-		Write-host "# List all Domain Computers" |  Out-File -FilePath $filePath -Append
-		$computers = Get-ADComputer -Filter * | Select-Object Name
-		foreach ($user in $computers) {
-			$name = $computer.Name
-			$name | Out-File -Append -FilePath domainComputers.txt
-			$objects += $name
-		}
-	}
- else {
-		Write-Host "Invalid object type. Please specify 'User' or 'Computer'."
-	}
- return $objects
-}
-function DownloadACE{
-	param(
-		[string]$object,
-		[bool]$User #set this to true to donwload users
-	)
-	$delimiter="."
-	$targetDomain = $DomainName -split $delimiter
-	Write-Host "_________NEW-OBJECT" |  Out-File -FilePath "ACES.txt" -Append
-	if($user){
-		dsacls "CN="+$object+"CN=Users,DC="+$targetDomain[0]+",DC="+$targetDomain[1] |  Out-File -FilePath "ACES.txt" -Append
-	}
-	else{
-		dsacls "CN="+$object+"CN=Computers,DC="+$targetDomain[0]+",DC="+$targetDomain[1] | Out-File -FilePath "ACES.txt" -Append
-	}
-	
-
-}
-function RetrieveObject{#retrieve Objects(users and computers)
-	param(
-		[string]$filename
-	)
-	$filePath = $filename
-	$fileContent = Get-Content -Path $filePath
-	return $fileContent
 }
